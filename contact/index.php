5ce98c1fc6f3ef4f2d5c5ba0e9627253a452a58d
cedrickjames <cedrickorozo@gmail.com>
2023-09-03 21:38:40 +0800

    add google analytics

--- contact/index.php
+++ contact/index.php
@@ -66,6 +66,14 @@ echo '<script>alert("Message could not be sent. Mailer Error")</script>';
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N5PFTTLRPF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N5PFTTLRPF');
+</script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
